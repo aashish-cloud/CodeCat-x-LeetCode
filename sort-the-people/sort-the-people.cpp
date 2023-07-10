d2b7eecfa5cb7d2fec2d4af398cd33021451ae78
Aashish Aggarwal <64294095+aashish-cloud@users.noreply.github.com>
2023-07-10 16:18:38 +0530

    Time: 44 ms (46.99%) | Memory: 22 MB (76.98%) - LeetSync

--- sort-the-people/sort-the-people.cpp
+++ sort-the-people/sort-the-people.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
-        vector<int> bucket(100001, -1);
+        map<int, int> bucket;
 
         for(int i = 0; i < heights.size(); i++)
             bucket[heights[i]] = i;
         
         vector<string> ans;
 
-        for(int i = 100000; i >= 1; i--)
-            if(bucket[i] > -1)
-                ans.push_back(names[bucket[i]]);
+        for(auto itr = bucket.rbegin(); itr != bucket.rend(); itr++)
+            ans.push_back(names[itr->second]);
 
         return ans;
     }
