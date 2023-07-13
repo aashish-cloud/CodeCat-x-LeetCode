3c8767a234a7492e48ca112b331b8229a9ba1a09
Aashish Aggarwal <64294095+aashish-cloud@users.noreply.github.com>
2023-07-13 13:13:11 +0530

    Time: 30 ms (80.74%) | Memory: 7.4 MB (95.62%) - LeetSync

--- minimize-string-length/minimize-string-length.cpp
+++ minimize-string-length/minimize-string-length.cpp
@@ -2,16 +2,10 @@ class Solution {
 public:
     int minimizedStringLength(string s) {
         int count[26] = {0};
-        int n = s.size();
         int ans = 0;
 
-        for(int i = 0; i < n; i++) {
-            count[s[i] - 'a']++;
-        }
-
-        for(int cnt : count)
-            if(cnt)
-                ans++;
+        for(char c : s)  count[c - 'a']++;
+        for(int cnt : count)    if(cnt) ans++;
         
         return ans;
     }
