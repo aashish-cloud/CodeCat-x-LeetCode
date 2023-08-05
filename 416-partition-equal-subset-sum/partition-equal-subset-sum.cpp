333e4eab498f1e5f6fc2fef00edcaea3261ea641
Aashish Aggarwal <64294095+aashish-cloud@users.noreply.github.com>
2023-08-06 02:29:32 +0530

    Time: 239 ms (76.72%) | Memory: 94.1 MB (50.70%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -8,29 +8,33 @@ public:
         if(sum % 2 == 1)    return false;
         
         int k = sum / 2;
-        vector<vector<int>> dp(n, vector<int> (k + 1, -1));
+        vector<int> prev(k + 1, -1);
 
-        dp[n - 1][0] = 1;
+        prev[0] = 1;
         for(int j = 1; j <= k; j++)
-            if(nums[n - 1] - j == 0)    dp[n - 1][j] = 1;
-            else dp[n - 1][j] = 0;
+            if(nums[n - 1] - j == 0)    prev[j] = 1;
+            else prev[j] = 0;
         
         for(int i = n - 2; i >= 0; i--) {
+            vector<int> curr(k + 1, -1);
+
             for(int j = 0; j <= k; j++) {
                 if(j == 0) {
-                    dp[i][j] = 1;
+                    curr[j] = 1;
                     continue;
                 }
 
                 bool take = 0;
                 if(j - nums[i] >= 0)
-                    take = dp[i + 1][j - nums[i]];
-                bool notTake = dp[i + 1][j];
+                    take = prev[j - nums[i]];
+                bool notTake = prev[j];
 
-                dp[i][j] = take | notTake;
+                curr[j] = take | notTake;
             }
+
+            prev = curr;
         }
 
-        return dp[0][k];
+        return prev[k];
     }
 };
