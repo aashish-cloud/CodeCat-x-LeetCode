c0284927c20700ed00537045c72f23c2d912a19c
Aashish Aggarwal <64294095+aashish-cloud@users.noreply.github.com>
2023-08-06 16:59:53 +0530

    Time: 31 ms (52.88%) | Memory: 18.1 MB (68.20%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-        vector<vector<int>> dp(coins.size(), vector<int> (amount + 1, 0));
+        vector<int> prev(amount + 1, 0);
         int n = coins.size();
 
-        dp[n - 1][0] = 1;
+        prev[0] = 1;
         for(int j = 1; j <= amount; j++)
-            if(j - coins[n - 1] == 0)   dp[n - 1][j] = 1;
-            else dp[n - 1][j] = j - coins[n - 1] >= 0 ? dp[n - 1][j - coins[n - 1]] : 0;
+            if(j - coins[n - 1] == 0)   prev[j] = 1;
+            else prev[j] = j - coins[n - 1] >= 0 ? prev[j - coins[n - 1]] : 0;
 
         for(int i = n - 2; i >= 0; i--) {
+            vector<int> curr(amount + 1, 0);
             for(int j = 0; j < amount + 1; j++) {
                 int take = 0;
                 if(j - coins[i] >= 0)
-                    take = dp[i][j - coins[i]];
-                int notTake = dp[i + 1][j];
+                    take = curr[j - coins[i]];
+                int notTake = prev[j];
 
-                dp[i][j] = take + notTake;
+                curr[j] = take + notTake;
             }
+            prev = curr;
         }
 
-        return dp[0][amount];
+        return prev[amount];
     }
 };
