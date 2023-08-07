5f630a00bd1487ad0dab1a2d76b880bd4a9dd955
Aashish Aggarwal <64294095+aashish-cloud@users.noreply.github.com>
2023-08-07 23:42:22 +0530

    Time: 3 ms (82.89%) | Memory: 9.5 MB (19.73%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -4,16 +4,16 @@ public:
         int m = matrix.size();
         int n = matrix[0].size();
         
-        int row = 0;
-        int col = n - 1;
+        int l = 0, r = m*n - 1;
 
-        while(row < m && col > -1) {
-            if(target == matrix[row][col])    return true;
+        while(l <= r) {
+            int mid = (l + r) / 2;
+            if(target == matrix[mid / n][mid % n])    return true;
             
-            if(target > matrix[row][col]) 
-                row++;
+            if(target > matrix[mid / n][mid % n]) 
+                l = mid + 1;
             else
-                col--;
+                r = mid - 1;
         }
 
         return false;
