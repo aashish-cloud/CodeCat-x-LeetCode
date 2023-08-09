0b1d4989e9ac7f694a8f17f838381a216c1ec634
Aashish Aggarwal <64294095+aashish-cloud@users.noreply.github.com>
2023-08-10 03:15:36 +0530

    Time: 53 ms (94.69%) | Memory: 32.2 MB (61.38%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
-        map<int, vector<vector<int>>> m;
+        vector<vector<int>> ans;
         sort(arr.begin(), arr.end());
 
+        int mini = INT_MAX;
         for(int i = 1; i < arr.size(); i++) {
-            m[arr[i] - arr[i - 1]].push_back({arr[i - 1], arr[i]});
+            mini = min(mini, arr[i] - arr[i - 1]);
         }
 
-        return m.begin()->second;
+        for(int i = 1; i < arr.size(); i++) {
+            if(arr[i] - arr[i - 1] == mini)
+                ans.push_back({arr[i - 1], arr[i]});
+        }
+
+        return ans;
     }
 };
