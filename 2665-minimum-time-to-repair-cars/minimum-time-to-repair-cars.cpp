5e2f239984310f24764ac9d476361ac6a8a3adff
Aashish Aggarwal <64294095+aashish-cloud@users.noreply.github.com>
2023-08-10 12:00:09 +0530

    Time: 81 ms (88.92%) | Memory: 53.9 MB (13.65%) - LeetSync

--- 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.cpp
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
     long long repairCars(vector<int>& ranks, int cars) {
-        long long l = 0, r = 1e15;
-        long long ans = 0;
+        long long l = 0, r = 1e14;
+
         while(l < r) {
-            long long mid = (l + r) / 2;
-            
-            long long washed = 0;
+            long long mid = (l + r) / 2, washed = 0;
             for(int rank : ranks)
                 washed += sqrt(mid/rank);
 
-            cout << washed << " ";
-
-            if(washed >= cars) {
-                ans = mid;
-                r = mid;
-            } else {
-                l = mid + 1;
-            }
+            if(washed >= cars)  r = mid;
+            else    l = mid + 1;
         }
 
-        return ans;
+        return l;
     }
 };
