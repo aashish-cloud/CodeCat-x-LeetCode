9d199cb5a3637675e230633a586f50cf7959e97b
Aashish Aggarwal <64294095+aashish-cloud@users.noreply.github.com>
2023-08-14 01:41:58 +0530

    Time: 134 ms (81.35%) | Memory: 91.4 MB (59.63%) - LeetSync

---
+++ 2443-check-if-there-is-a-valid-partition-for-the-array/check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int isValid(int i, vector<int>& nums, vector<int>& dp) {
+        if(i == nums.size())    return dp[i] = 1;
+        if(i > nums.size())    return 0;
+
+        if(dp[i] != -1)     return dp[i];
+
+        if(isValid(i + 2, nums, dp)) {
+            if(nums[i] == nums[i + 1])  return dp[i] = 1;
+        }
+        
+        if(isValid(i + 3, nums, dp)) {
+            bool isConsecutive = nums[i] + 1 == nums[i + 1] && nums[i + 1] + 1 == nums[i + 2];
+            bool isSame = nums[i] == nums[i + 1] && nums[i + 1] == nums[i + 2];
+            if(isConsecutive || isSame) return dp[i] = 1;
+        }
+
+        return dp[i] = 0;
+    }
+
+    bool validPartition(vector<int>& nums) {
+        vector<int> dp(nums.size() + 2, -1);
+        return isValid(0, nums, dp);
+    }
+};
