b0c6f800a758cc6dd10405fe3567f53858e04029
Aashish Aggarwal <64294095+aashish-cloud@users.noreply.github.com>
2023-08-16 16:19:42 +0530

    Time: 198 ms (92.42%) | Memory: 134.8 MB (36.59%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -6,15 +6,15 @@ public:
         deque<int> dq;
         
         for(int i = 0; i < k; i++) {
-            while(dq.size() && nums[i] > nums[dq.back()]) dq.pop_back();
+            while(dq.size() && nums[i] >= nums[dq.back()]) dq.pop_back();
             dq.push_back(i);
         }
 
         ans.push_back(nums[dq.front()]);
 
         for(int i = k; i < nums.size(); i++) {
-            while(dq.size() && dq.front() < (i - k + 1))   dq.pop_front();
-            while(dq.size() && nums[i] > nums[dq.back()]) dq.pop_back();
+            if(dq.size() && dq.front() == i - k)   dq.pop_front();
+            while(dq.size() && nums[i] >= nums[dq.back()]) dq.pop_back();
             dq.push_back(i);
 
             ans.push_back(nums[dq.front()]);
