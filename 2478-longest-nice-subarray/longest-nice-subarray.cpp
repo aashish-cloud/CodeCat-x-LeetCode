b101505bc9405805f07190e57b80381f820f4219
Aashish Aggarwal <64294095+aashish-cloud@users.noreply.github.com>
2023-08-24 02:16:59 +0530

    Time: 100 ms (85.58%) | Memory: 57.1 MB (59.01%) - LeetSync

--- 2478-longest-nice-subarray/longest-nice-subarray.cpp
+++ 2478-longest-nice-subarray/longest-nice-subarray.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int longestNiceSubarray(vector<int>& nums) {
-        int curr = 0, i = 0, ans = 1;
+        int current_group = 0, i = 0, ans = 1;
         for(int j = 0; j < nums.size(); j++) {
-            while(i < j && (curr & nums[j]) != 0)  curr &= ~nums[i++];
-            curr |= nums[j];
+            while(i < j && (current_group & nums[j]) != 0)  current_group &= ~nums[i++];
+            current_group |= nums[j];
             ans = max(ans, j - i + 1);
         }
 
